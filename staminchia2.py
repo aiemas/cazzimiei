c2a269e815b93abefe047338ea2c7fd2f47f7471
aiemas <sbiancucci82@gmail.com>
2026-08-28 13:35:51 +0200

    Update URL to include category and events

--- staminchia2.py
+++ staminchia2.py
@@ -9,7 +9,7 @@ import os
 # CONFIGURAZIONE
 # ============================================================
 
-URL = "https://dlstreams.st"
+URL = "https://dlstreams.st/index.php?cat=All+Soccer+Events+%E2%9A%BD"
 
 CHANNELS_URL = "https://dlstreams.st/24-7-channels.php"
 
